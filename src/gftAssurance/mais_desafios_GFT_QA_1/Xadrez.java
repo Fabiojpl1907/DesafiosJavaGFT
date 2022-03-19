207b739c219ba4724933b654f4a1a3161af1ebff
Fabiojpl1907 <fabiojpl.1907@gmail.com>
2022-03-19 17:19:31 -0300

    Refatorar Codigos

--- src/gftAssurance/mais_desafios_GFT_QA_1/Xadrez.java
+++ src/gftAssurance/mais_desafios_GFT_QA_1/Xadrez.java
@@ -43,3 +43,16 @@ public class Xadrez {
         sc.close();
     }
 }
+
+// outra solução
+//import java.util.Scanner;
+//
+//public class Main{
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int L = sc.nextInt();
+//        int C = sc.nextInt();
+//        System.out.println(((L + C) % 2 == 0) ? "1" : "0");
+//        sc.close();
+//    }
+//}
